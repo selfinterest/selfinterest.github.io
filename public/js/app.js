1dc598be695d17b0e2e0c8aaf6c505f74049b441
Terrence <parliamentaryterrence@gmail.com>
2014-06-22 18:20:11 -0600

    Again

--- public/js/app.js
+++ public/js/app.js
@@ -11,13 +11,9 @@ angular.module("TerrenceWatson", ["ui.router"])
                     "posts": ["$http", "$rootScope", "$q", function($http, $rootScope, $q){
                         var deferred = $q.defer();
                         $http.get("/posts.json").success(function(data){
-<<<<<<< HEAD
+
                             $rootScope.data = data[0];
                             deferred.resolve(data[0].posts);
-=======
-                            $rootScope.posts = data;
-                            deferred.resolve(data);
->>>>>>> parent of 05f91bb... About to change template
                         });
                         return deferred.promise;
                     }]
@@ -27,12 +23,8 @@ angular.module("TerrenceWatson", ["ui.router"])
     .controller("AppCtrl", ["$scope", "$http", function($scope, $http){
         
     }])
-<<<<<<< HEAD
+
     .controller("IndexCtrl", ["$scope", "posts", function($scope, posts){
         console.log(posts);
         $scope.posts = posts;
-=======
-    .controller("IndexCtrl", ["$scope", function($scope){
-        
->>>>>>> parent of 05f91bb... About to change template
     }])
